2e6cd00063fe677ee08d5546df4052259170ab46
Umer Farooq <holypulse@gmail.com>
2022-12-25 13:20:28 +0000

    test12

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const app: Express = express();
 
 app.use(helmet());
